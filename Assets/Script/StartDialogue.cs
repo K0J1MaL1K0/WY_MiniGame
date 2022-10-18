f7a9c322fbdee8440e49ad6a550755fdb04f8e2d
Peter.Li <1416050163@qq.com>
2022-10-18 21:35:08 +0800

    主线故事更改

--- Assets/Script/StartDialogue.cs
+++ Assets/Script/StartDialogue.cs
@@ -7,8 +7,30 @@ public class StartDialogue : MonoBehaviour
 {
     public DialogueTreeController dialogue;
     public Inventory item;
+    public Collider2D coll;
+
+ 
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+    private void Update()
+    {
+        sendMail();
+    }
+
+    private void sendMail()
+    {
+        if(coll.tag == "xin")
+        {
+            //dialogue.StartDialogue();
+            Debug.Log("´¥·¢¶Ô»°£¡");
+        }
+    }
+    
     private void OnMouseOver()
     {
+        
         if (Input.GetMouseButton(0))
         {
 
@@ -16,11 +38,5 @@ public class StartDialogue : MonoBehaviour
             dialogue.StartDialogue();
 
         }
-        
-
-
     }
-
-
-   
 }
