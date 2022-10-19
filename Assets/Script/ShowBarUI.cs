461d5228019d2a289def956bdbfd842fdeec1bb0
Lzh20122102 <2511915139@qq.com>
2022-10-19 23:04:54 +0800

    修复信件不能跟随物品栏下滑的bug

--- Assets/Script/ShowBarUI.cs
+++ Assets/Script/ShowBarUI.cs
@@ -5,9 +5,9 @@ using UnityEngine.EventSystems;
 
 public class ShowBarUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public float speed = 5;
-    public GameObject MoveBar;
-    public float showBarTime = 3;
+    public float speed = 5;  //显示速度
+    public GameObject MoveBar;  //物品栏父节点GO
+    public float showBarTime = 3;  //显示物品栏的计时
 
     float timer;
     bool showBar = false;
@@ -21,25 +21,25 @@ public class ShowBarUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
     // Update is called once per frame
     void Update()
     {
+        //利用物品栏的位置判断是否显示
         if (showBar && MoveBar.transform.position.y <= -2.5)
             MoveBar.transform.position -= new Vector3(0, -2.5f, 0) * speed * Time.deltaTime;
         else if (!showBar && MoveBar.transform.position.y >= -5 && timer < 0)
             MoveBar.transform.position += new Vector3(0, -2.5f, 0) * speed * Time.deltaTime;
 
-        print(timer);
-        if(!showBar && MoveBar.transform.position.y > -3)
+        if(!showBar && MoveBar.transform.position.y > -3)  //计时
             timer -= Time.deltaTime;
-        else if(showBar)
+        else if(showBar)  //重置计时器
             timer = showBarTime;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        showBar = true;
+        showBar = true;  //显示物品栏
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        showBar = false;
+        showBar = false;  //关闭物品栏
     }
 }
